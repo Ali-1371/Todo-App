d504fc062cad34ac87e6f0c4e95306f646f1c4d8
Ali <shokrollahiali7@gmail.com>
2022-02-06 00:43:53 +0330

    Config axios

--- todo-app/src/configs/constants.js
+++ todo-app/src/configs/constants.js
@@ -1,3 +1,10 @@
 export const constant = {
   minNumberOfLetters: 3,
+
+  restfulVerbs: {
+    get: 'GET',
+    post: 'POST',
+    put: 'PUT',
+    delete: 'DELETE',
+  },
 };
---
+++ todo-app/src/services/httpRequest.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { constant } from '../configs/constants';
+
+const httpRequest = (method, url, params, data) => {
+  const axiosInstance = axios.create({
+    baseURL:
+      'https://react-todo-app-f2db9-default-rtdb.asia-southeast1.firebasedatabase.app/',
+    timeout: 10000,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return axiosInstance({ method, url, params, data });
+};
+
+export const getRequest = (url, params) =>
+  httpRequest(constant.restfulVerbs.get, url, params);
+
+export const postRequest = (url, data) =>
+  httpRequest(constant.restfulVerbs.post, url, {}, data);
+
+export const putRequest = (url, data) =>
+  httpRequest(constant.restfulVerbs.put, url, {}, data);
+
+export const deleteRequest = (url) =>
+  httpRequest(constant.restfulVerbs.delete, url);
