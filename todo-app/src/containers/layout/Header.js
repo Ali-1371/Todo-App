bbd9996e43822ee1011565cd8757a9a1f6121ef6
Ali <shokrollahiali7@gmail.com>
2022-02-05 17:42:38 +0330

    Add navlinks to header --- Header styling

--- todo-app/src/containers/layout/Header.js
+++ todo-app/src/containers/layout/Header.js
@@ -1,5 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import { viewLinks } from '../../configs/links';
+
+const navLinks = [
+  { title: 'Todo list', path: viewLinks.todoList },
+  { title: 'Add a task', path: viewLinks.todoAdd },
+];
+
 const Header = () => {
-  return <div>HEADER</div>;
+  return (
+    <header className='header'>
+      <h1 className='me-auto'>Task management</h1>
+      <nav className='header_nav'>
+        <ul className='d-flex m-0 p-0'>
+          {navLinks.map((navLink) => (
+            <li key={navLink.path}>
+              <NavLink to={navLink.path} exact activeClassName='active'>
+                {navLink.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
